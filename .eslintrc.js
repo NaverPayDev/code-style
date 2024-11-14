64f46b720929ae69c5bfe11aa005a18d900bd7f1
yujeong-jeon <yujeong.jeon@navercorp.com>
2024-11-14 19:55:42 +0900

    [#57] eslint module 임시 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-    extends: '@naverpay/eslint-config/node',
+    env: {
+        node: true,
+        es6: true,
+        es2023: true,
+        commonjs: true,
+    },
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2023,
+    },
 }
