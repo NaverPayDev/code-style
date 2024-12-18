4c08af6d88678a3e5b69056d639d1ae95b8c8d12
yongholeeme <yongholeeme@gmail.com>
2024-12-18 23:13:51 +0900

    [#58] ✨ Add vitest gloabals

--- packages/eslint-config/react/index.js
+++ packages/eslint-config/react/index.js
@@ -13,6 +13,7 @@ export default [
             globals: {
                 ...globals.browser,
                 ...globals.jest,
+                ...globals.vitest,
                 ...globals.es2023,
             },
         },
