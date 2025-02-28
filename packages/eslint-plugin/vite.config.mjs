5f6bbf017be56c4b55d5a75acc857c8f8f2c733b
hyunseok-kim-f <hyunseok.kim_f@navercorp.com>
2025-02-28 16:14:06 +0900

    [#94] eslint-plugin pite 적용

---
+++ packages/eslint-plugin/vite.config.mjs
@@ -0,0 +1,14 @@
+import {createViteConfig} from '@naverpay/pite'
+import builtins from 'builtin-modules'
+
+export default createViteConfig({
+    cwd: '.',
+    entry: ['./lib/index.js'],
+    ignoredPolyfills: ['esnext.json.parse', 'es.array.push'],
+    options: {
+        minify: false,
+        rollupOptions: {
+            external: [...builtins],
+        },
+    },
+})
