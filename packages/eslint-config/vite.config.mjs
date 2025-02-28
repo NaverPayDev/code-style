eec954ee96f33c471b2bd9d3a0afd271d0ac48db
hyunseok-kim-f <hyunseok.kim_f@navercorp.com>
2025-02-28 17:17:05 +0900

    [#94] 기본 cwd 사용

--- packages/eslint-config/vite.config.mjs
+++ packages/eslint-config/vite.config.mjs
@@ -2,7 +2,6 @@ import {createViteConfig} from '@naverpay/pite'
 import builtins from 'builtin-modules'
 
 export default createViteConfig({
-    cwd: '.',
     entry: ['./index.js'],
     options: {
         minify: false,
--- packages/eslint-plugin/vite.config.mjs
+++ packages/eslint-plugin/vite.config.mjs
@@ -2,7 +2,6 @@ import {createViteConfig} from '@naverpay/pite'
 import builtins from 'builtin-modules'
 
 export default createViteConfig({
-    cwd: '.',
     entry: ['./lib/index.js'],
     ignoredPolyfills: ['esnext.json.parse', 'es.array.push'],
     options: {
